ef2c2b101bc1149a7d6a464d3323781d1cf0241c
Cariad Eccleston <cariad@cariad.earth>
2024-09-03 08:46:16 +0100

    Exercise: Setting up the HTML document, step 3

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,17 @@
+import HeaderMenu from './HeaderMenu';
+import logo from './images/horizontal-logo.png';
+
 function Header() {
   return (
     <header>
-      <nav>
-      </nav>
+      <img
+        alt='Little Lemon'
+        height={61}
+        src={logo}
+        width={200}
+      />
+
+      <HeaderMenu />
     </header>
   );
 }
